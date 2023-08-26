e5adb25bd738ca5f26ed655d91a48c9f256d9f47
MikiTwenty <97092783+MikiTwenty@users.noreply.github.com>
2023-08-26 13:37:44 +0200

    Comments fix

--- imgdt/utils.py
+++ imgdt/utils.py
@@ -1,7 +1,7 @@
 # utils.py
 # Copyright (C) 2023 Michele Ventimiglia (michele.ventimiglia01@gmail.com)
 #
-# This module is part of ImageDatasetTools and is released under
+# This module is part of imgdt and is released under
 # the MIT License: https://opensource.org/license/mit/
 
 class _STDOFormat:
